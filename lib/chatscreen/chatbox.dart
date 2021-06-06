593a06769d0fddca0d1ea57a7b1565204bdd1552
MikeWski <pheaem@puc.edu.kh>
2021-06-06 18:23:08 +0700

    chat push

---
+++ lib/chatscreen/chatbox.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatBox extends StatefulWidget {
+  const ChatBox({key}) : super(key: key);
+
+  @override
+  _ChatBoxState createState() => _ChatBoxState();
+}
+
+class _ChatBoxState extends State<ChatBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
